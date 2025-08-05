5237d10e0bb71bb28916c7321be4ed7d03196bee
Gasta88 <gasta88@hotmail.com>
2025-08-05 21:24:47 +0200

    added some code

--- src/utils/data_quality_limits.py
+++ src/utils/data_quality_limits.py
@@ -1,6 +1,28 @@
 columns_limits = {
     "Territorio": {
-        "valid_values": []
+        "valid_values": [
+            "ITC1",
+            "ITC2",
+            "ITC3",
+            "ITC4",  # Nord-Ovest
+            "ITH1",
+            "ITH2",
+            "ITH3",
+            "ITH4",
+            "ITH5",  # Nord-Est
+            "ITI1",
+            "ITI2",
+            "ITI3",
+            "ITI4",  # Centro
+            "ITF1",
+            "ITF2",
+            "ITF3",
+            "ITF4",
+            "ITF5",
+            "ITF6",  # Sud
+            "ITG1",
+            "ITG2",  # Isole
+        ]
     },
     "Tempo": {
         "valid_values": []
